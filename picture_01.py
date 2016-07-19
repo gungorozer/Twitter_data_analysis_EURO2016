51542e0d63b990f3a24e2c43cb3cd7e18fa89525
Gungor Ozer <gungor.ozer@gmail.com>
2016-07-18 20:44:22 -0400

    02

--- picture_01.py
+++ picture_01.py
@@ -2,16 +2,11 @@ from bokeh.charts import Dot, Scatter, Histogram, output_file, show
 from bokeh.plotting import figure, show, output_file
 from bokeh.io import push_notebook
 from bokeh.models import SingleIntervalTicker, LinearAxis
-#from bokeh.sampledata.autompg import autompg as df
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv("https://sites.google.com/site/gungorozer/data-science/euro2016_tweets.bytime")
 
-#print df['Ronaldo']
-
-#Minutes_After_Kickoff,Total_by_Time,Portugal,France,Ronaldo,Griezmann,Eder,Goal,Post
-#df.sort('Ronaldo', inplace=True)
 
 plot = Dot(df,values='Ronaldo', label='Minutes_After_Kickoff',
            title="Python Interpreter Sampling",
@@ -25,16 +20,6 @@ plot.add_layout(xaxis, 'below')
 plot.add_layout(yaxis, 'left')
 plot.xaxis.axis_label = 'Minutes after kickoff'
 plot.yaxis.axis_label = 'Count of Ronaldo words'
-
-
-#plot = figure(width=1200, height=600)
-#plot.rect(x=df['Minutes_After_Kickoff'], y=df['Ronaldo'], width=10, height=df['Ronaldo'], color="black",
-#    width_units="screen", height_units="screen")
-
-#hist = Scatter(df,x='Minutes_After_Kickoff',y=['Ronaldo','Griezmann'], 
-#               title="Identifying important events", legend='top_right')
-# hist = Histogram(df, values='Ronaldo', color='black',
-#title="Identifying important events", legend='top_right')
 
 output_file("important_events.html", title="Important_events.py")
 
